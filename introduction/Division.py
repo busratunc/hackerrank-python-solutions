41304b732e90654d94366c9f0fc48cc32309afcf
busratunc <126500221+busratunc@users.noreply.github.com>
2026-03-11 00:21:56 +0300

    Create Division.py
    Add division solution

---
+++ introduction/Division.py
@@ -0,0 +1,11 @@
+# HackerRank Problem: Division
+# Domain: Python
+# Topic: introduction
+# Difficulty: Easy
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)

637126fbf8a1b496401345fc38d662827a2d9a09
busratunc <126500221+busratunc@users.noreply.github.com>
2026-03-11 23:16:13 +0300

    Create ArithmeticOperators.py
    add Arithmetic Operators problem

---
+++ introduction/ArithmeticOperators.py
@@ -0,0 +1,10 @@
+# HackerRank Problem: Arithmetic Operators
+# Domain: Python
+# Topic: Introduction
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print (a+b)
+    print (a-b)
+    print (a*b)
